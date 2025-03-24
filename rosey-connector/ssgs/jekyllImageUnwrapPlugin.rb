af4b92f1f2518e40a16928685889492394d61176
tomrcc <tom@cloudcannon.com>
2025-03-25 10:09:49 +1300

    Add to readme, example config, organise plugins

--- rosey-connector/ssgs/jekyllImageUnwrapPlugin.rb
+++ rosey-connector/ssgs/jekyllImageUnwrapPlugin.rb
@@ -1,5 +1,6 @@
 require 'kramdown/converter/html'
 
+# Prevents images from being wrapped in a <p> tag, meaning they don't get mistakenly tagged for translation
 module StandaloneImages
   def convert_p(el, indent)
     return super unless el.children.size == 1 && (el.children.first.type == :img || (el.children.first.type == :html_element && el.children.first.value == "img"))
